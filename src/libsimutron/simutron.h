46312964ad7f91768b819b5c48cac7ef6b521723
Carl Schwope <schwope.carl@gmail.com>
2013-12-09 22:06:59 -0500

    update simutron.h

--- src/libsimutron/simutron.h
+++ src/libsimutron/simutron.h
@@ -1,16 +1,18 @@
 #ifndef SIMUTRON_H
 #define SIMUTRON_H
 
-#include "city.h"
-#include "budget.h"
-#include "buyable.h"
-#include "zone.h"
-#include "growable.h"
-
 /**
 * @namespace Simutron
 * @brief Prvide a namespace for libsimutron
 */
 namespace Simutron {}
+
+#include "city.h"
+#include "budget.h"
+#include "buyable.h"
+#include "zone.h"
+#include "buildable.h"
+#include "growable.h"
+#include "plopable.h"
 
 #endif
